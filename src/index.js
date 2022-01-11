98556c6e0de505f7d51a8a4f21cfe0427bf30590
fujimon-0511 <95519718+fujimon-0511@users.noreply.github.com>
2022-01-11 23:45:30 +0900

    セクション4-20

--- src/index.js
+++ src/index.js
@@ -1,10 +1,52 @@
-import "./styles.css";
+/**
+ * constで定義したオブジェクトはプロパティの変更が可能
+ */
+// const val4 = {
+//   name: "ふじもん",
+//   age: 26
+// };
+// val4.name = "jak";
+// val4.addres = "Yamagata";
+// console.log(val4);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+/**
+ * constで定義した配列はプロパティの変更が可能
+ */
+// const val5 = ["dog", "cat"];
+// val5[0] = "bird";
+// val5.push("monkey");
+// console.log(val5);
+
+/**
+ * テンプレート文字列
+ */
+// const name = 'ふじもん';
+// const age = 26;
+// 私の名前はふじもんです。年齢は26歳です。
+
+//従来の方法
+// const messege1 = '私の名前は'　+ name +'です。年齢は'　+ age + '歳です。'
+// console.log(messege1);
+
+// テンプレート文字列を用いた方法
+// const messege2 = `私の名前は${name}です。年齢は${age}歳です。`;
+// console.log(messege2);
+
+/**
+ * アロー関数
+ */
+// 従来の関数
+// function func1(str) {
+//   return str;
+// }
+const func1 = function (str) {
+  return str;
+};
+console.log(func1("func1です"));
+
+// アロー関数
+const func2 = (str) => str;
+console.log(func2("func2です"));
+
+const func3 = (num1, num2) => num1 + num2;
+console.log(func3(10, 20));
